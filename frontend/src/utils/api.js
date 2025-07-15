f6a88136d9e24fbb088d3b9dc454232bf456a1b6
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2025-07-15 22:24:21 +0530

    Ensure all frontend and backend files are included

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,6 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://rithu-bl-web-side-api.vercel.app/";
 
 export const signup = async (data) => {
   const res = await fetch(`${API_URL}/users/signup`, {
