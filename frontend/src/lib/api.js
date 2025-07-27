5cb5f2cf72a914baafb78b3f0a3adafcc705c21b
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2025-07-27 20:14:17 +0530

    Fixed serverless function deployment

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "https://rithu-server-side-giwanthaathukoralas-projects.vercel.app/api";
+  "https://rithu-server-side-mwujl43va-giwanthaathukoralas-projects.vercel.app/api";
 
 // Create axios instance
 const api = axios.create({
