73451a0c975a58b8308c977af657baa1d1c8e015
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2025-08-22 16:47:24 +0530

    Database new conntion

--- backend/server.js
+++ backend/server.js
@@ -79,9 +79,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path}`);
