8c31222339c38e73c40a01ec0c7a814709d10a9d
GiwanthaAthukorala <giwantha@auslankatechnologies.org>
2025-11-24 08:17:47 +0530

    Link update

--- frontend/src/pages/Instagram-Verification/page.js
+++ frontend/src/pages/Instagram-Verification/page.js
@@ -375,7 +375,7 @@ export default function InstagramVerificationTask() {
                     </a>
                   </div>{" "}
                    */}
-                  {/* <div className="mt-6 bg-gradient-to-r from-pink-500 via-rose-500 to-red-500 p-6 rounded-2xl shadow-xl">
+                  <div className="mt-6 bg-gradient-to-r from-pink-500 via-rose-500 to-red-500 p-6 rounded-2xl shadow-xl">
                     <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
                       <div className="flex-1">
                         <h4 className="font-black text-white mb-2 text-lg flex items-center gap-2">
@@ -383,12 +383,12 @@ export default function InstagramVerificationTask() {
                           Follow Our Main Account
                         </h4>
                         <p className="text-white/90 text-sm">
-                          ඉන්ස්ට්‍රග්‍රෑම් එකවුන්ට් එක ෆලෝ කරන්න •Japan Cosmetic
-                          Mart LK
+                          ඉන්ස්ට්‍රග්‍රෑම් එකවුන්ට් එක ෆලෝ කරන්න • Dimentro Sri
+                          Lanka
                         </p>
                       </div>
                       <a
-                        href="https://www.instagram.com/japan_cosmetic_mart_lk?igsh=MW5ybnllcmMyYWQ5Ng=="
+                        href="https://www.instagram.com/dimentro.sl?igsh=MTg1a2c2Ymg4ZnRmNg=="
                         target="_blank"
                         rel="noopener noreferrer"
                         className="flex items-center justify-center bg-white text-pink-600 px-6 py-3 rounded-xl hover:bg-pink-50 transition-all duration-200 font-bold shadow-lg hover:shadow-xl transform hover:scale-105"
@@ -398,7 +398,7 @@ export default function InstagramVerificationTask() {
                       </a>
                     </div>
                   </div>{" "}
-                    */}
+                  {/*   */}
                 </div>
               </div>
 
